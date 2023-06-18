7c0085345a03f98febbec21d60c166acd2ee9be9
Vishesh Garg <vishesh.garg@vishesh.garg-C02GG1VCML7H>
2023-06-18 13:16:15 +0530

    links to github and codemirror

--- components/button.js
+++ components/button.js
@@ -6,15 +6,17 @@ export default function Button(props) {
   const { themeStyles } = useCustomTheme();
   const classname = "hover : bg-cyan-900";
   return (
-    <button
-      className={themeStyles.classButton}
-      style={{
-        cursor: "pointer",
-        color: themeStyles.col02.color,
-        fontFamily: themeStyles.font,
-      }}
-    >
-      {props.index}
-    </button>
+    <a href={props.href}>
+      <div
+        className={themeStyles.classButton}
+        style={{
+          cursor: "pointer",
+          color: themeStyles.col02.color,
+          fontFamily: themeStyles.font,
+        }}
+      >
+        {props.index}
+      </div>
+    </a>
   );
 }
