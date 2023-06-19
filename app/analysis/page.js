4881ba090c85da82e0d5c3b08e1a50122cb59adf
Anantha krishnan <anantha.krishnan@anantha.krishnan-C02DR5YSMD6N>
2023-06-19 11:42:25 +0530

    ui-changes, autocomplete bug fix

--- app/analysis/page.js
+++ app/analysis/page.js
@@ -3,9 +3,10 @@ import React, { useState, useContext } from "react";
 import { useCustomDirection } from "@/context/useDirectionHook";
 import BasicLayout from "../../components/basicLayout";
 import MainAnalysis from "../../components/mainAnalysis";
+import constants from "@/utils/constants";
 export default function Home() {
   const { direction } = useCustomDirection();
-  if (direction === "LeftToRight") {
+  if (direction === constants.LEFT_TO_RIGHT) {
     return (
       <div dir="ltr">
         <BasicLayout>
