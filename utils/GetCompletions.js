bc90d29b87a738ec66ea823ec508575633a5e70b
Anantha krishnan <anantha.krishnan@anantha.krishnan-C02DR5YSMD6N>
2023-07-03 14:53:24 +0530

    Open popup on selecting keywords

--- utils/GetCompletions.js
+++
@@ -1,9 +0,0 @@
-const Completions = (items) => {
-  return items.map((item) => ({
-    label: item.label,
-    type: item.type,
-    info: item.info,
-    apply: item.apply,
-  }));
-};
-export default Completions;
--- utils/IsValidSelection.js
+++ utils/IsValidSelection.js
@@ -1,4 +1,9 @@
 import { ResearchAdvanceQLLexer } from "@/components/antlrGenerated";
+import Data from "./Data";
+const keywords = new Set();
+Data.keywords.forEach((item) => {
+  keywords.add(item.label);
+});
 const IsValidSelection = (tokens, startPos, endPos) => {
   let actualStartPos = startPos,
     actualEndPos = endPos,
@@ -8,6 +13,17 @@ const IsValidSelection = (tokens, startPos, endPos) => {
   tokens.forEach((token) => {
     if (
       token.type === ResearchAdvanceQLLexer.PHRASE &&
+      token.startIndex <= startPos &&
+      token.stopIndex >= endPos
+    ) {
+      actualSelectedText = token.text;
+      actualStartPos = token.startIndex;
+      actualEndPos = token.stopIndex;
+      isValidSelection = true;
+    }
+    if (
+      token.type === ResearchAdvanceQLLexer.TERM_NORMAL &&
+      keywords.has(token.text) &&
       token.startIndex <= startPos &&
       token.stopIndex >= endPos
     ) {
