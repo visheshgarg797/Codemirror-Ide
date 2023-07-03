9d291f39cd7a52078df92b631bf4edd2b547115e
Anantha krishnan <anantha.krishnan@anantha.krishnan-C02DR5YSMD6N>
2023-07-03 17:05:47 +0530

    Merge

--- components/DirectionChangeButton.js
+++ components/DirectionChangeButton.js
@@ -2,34 +2,38 @@
 import React from "react";
 import { useCustomDirection } from "@/context/useDirectionHook";
 import { useCustomTheme } from "../context/useThemeHook";
-import { BsArrowLeftRight } from "react-icons/bs";
+import { Theme_Name } from "@/constants/ThemeName";
 
 const DirectionChangeButton = () => {
-  const { direction, toggleDirection } = useCustomDirection();
+  const { toggleDirection } = useCustomDirection();
   const { themeStyles } = useCustomTheme();
 
-  let name = "";
-  if (direction == "ltr") {
-    name = "RTL";
-  } else {
-    name = "LTR";
-  }
   return (
-    <button
-      onClick={toggleDirection}
-      className={themeStyles.classButton}
-      style={{
-        cursor: "pointer",
-        color: themeStyles.col02.color,
-        fontFamily: themeStyles.font,
-        marginInlineStart: "20px",
-        borderRadius: "50%",
-        marginTop: "8px",
-        marginInlineEnd: "  12px",
-      }}
-    >
-      <BsArrowLeftRight size={15} />
-    </button>
+    <div style={{ marginTop: "4px", marginRight: "12px" }}>
+      <button
+        type="button"
+        className={`active:scale-95 transition-transform flex w-10 h-10 rounded-full items-center justify-center outline-link hover:bg-gray-${
+          themeStyles.theme === Theme_Name.DARK_MODE ? "500" : "300"
+        }`}
+        onClick={toggleDirection}
+      >
+        <svg
+          stroke={
+            themeStyles.theme === Theme_Name.DARK_MODE ? "white" : "black"
+          }
+          width="30"
+          height="30"
+          viewBox="-2 -2 28 28"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"
+          ></path>
+        </svg>
+      </button>
+    </div>
   );
 };
 
