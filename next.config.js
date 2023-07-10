29eafa1cd817b0e8f39f68adf7b0e2260957d9ac
Vishesh Garg <vishesh.garg@vishesh.garg-C02GG1VCML7H>
2023-07-10 11:49:11 +0530

    api key updated

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 require("dotenv").config();
-const nextConfig = {};
+const nextConfig = {
+  env: { NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY },
+};
 
 module.exports = nextConfig;
